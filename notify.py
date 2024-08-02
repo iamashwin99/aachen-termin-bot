5bc62e2674b2d31538257454322ca9085a09cada
iamashwin99 <ashwin-kumar.karnad@mpsd.mpg.de>
2024-08-02 15:00:20 +0200

    use subprocess to lauch

--- notify.py
+++ notify.py
@@ -1,9 +1,10 @@
 from termin import aachen_hbf_termin
 import os
 import datetime
+import subprocess as sb
 audio = "/home/karnada/Downloads/Trash/mixkit-slot-machine-payout-alarm-1996.wav"
 website = "https://termine.staedteregion-aachen.de/auslaenderamt/select2?md=1"
-say = "new appointment avaialable."*15
+say = "new appointment avaialable."*5
 while True:
     for team in ['Team 1', 'Team 2', 'Team 3']:
         is_available, res = aachen_hbf_termin(team)
@@ -23,6 +24,7 @@ while True:
             date_already_booked = datetime.datetime(2024, 8, 26)
             if date_available < date_already_booked:
                 #os.system(f"xdg-open {audio}")
-                os.system(f"spd-say {say}")
+                #os.system(f"spd-say {say}")
                 os.system(f"xdg-open {website}")
+                sb.run(["spd-say", say])
                 exit(0)
